3bec6c258d916ad9048cbec478d5ccf3a0b25dad
Ofer Baharav <pair+ofer@videovivoapp.com>
2013-06-10 00:03:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'bc3c6465ca126b81f4e432da7f8f26670a67745bd2be6dc3f4bb5272c5604267d2350137711e0ec2968b9144008ce0d8f0af5726817bc049d0f0f2e58a239cb2'
